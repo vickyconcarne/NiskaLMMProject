dc4e33fa7642c5630117773ac5bcf2529a124348
vickyconcarne <39057304+vickyconcarne@users.noreply.github.com>
2021-10-27 20:42:56 +0200

    new formula address

--- Assets/Scripts/Menu/FormulaInfoSend.cs
+++ Assets/Scripts/Menu/FormulaInfoSend.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Networking;
 
 public class FormulaInfoSend : MonoBehaviour
 {
@@ -23,7 +24,7 @@ public class FormulaInfoSend : MonoBehaviour
     public TextMeshProUGUI loadingText;
 
     //private string BASE_URL = "https://docs.google.com/forms/u/5/d/e/1FAIpQLSce8xb_tjOwDYTPjJi0brAiGx1d08a_fJ2juMjPu2_oRC1USw/formResponse";
-    private string BASE_URL = "https://docs.google.com/forms/d/e/1FAIpQLSe3DQetXV9wG5DC6Fd8YggtaibHiGu__MLzPME39SoTbrmx5Q/viewform?usp=pp_url";
+    private string BASE_URL = "https://docs.google.com/forms/d/e/1FAIpQLSe3DQetXV9wG5DC6Fd8YggtaibHiGu__MLzPME39SoTbrmx5Q/formResponse";
     void Start()
     {
         highscore = RandomTileManager.instance.GetScore().ToString();
@@ -41,13 +42,14 @@ public class FormulaInfoSend : MonoBehaviour
         //formulaAnimator.SetTrigger("Loading");
         loadingText.text = "Envoi...";
         WWWForm form = new WWWForm();
-        form.AddField("entry.687036189", name);
-        form.AddField("entry.1938221334", reseau);
-        form.AddField("entry.1800075712", score);
+        form.AddField("entry.454267212", name);
+        form.AddField("entry.163988641", reseau); //1938221334
+        form.AddField("entry.1255323953", score); //1800075712
         byte[] rawData = form.data;
         WWW www = new WWW(BASE_URL, rawData);
+        //UnityWebRequest uwr = UnityWebRequest.Post(BASE_URL, form);
         yield return new WaitForSeconds(3.5f);
-        if(www.error != null)
+        if(www.error != null /*uwr.result == UnityWebRequest.Result.ConnectionError || uwr.result == UnityWebRequest.Result.ProtocolError*/)
         {
             formulaAnimator.SetTrigger("Fail");
             loadingText.text = "Erreur! Connexion introuvable";
@@ -58,6 +60,7 @@ public class FormulaInfoSend : MonoBehaviour
             formulaAnimator.SetTrigger("Success");
             loadingText.text = "Reussi!";
             yield return www;
+            //yield return uwr;
         }
         
     }
